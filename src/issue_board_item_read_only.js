0c564ad65d9fc90ae0de4b8dfb07a6b298726631
Madhava Hansen <madhavah@gmail.com>
2020-02-26 13:35:57 -0500

    destructure data props in read only mode

--- src/issue_board_item_read_only.js
+++ src/issue_board_item_read_only.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
 export const IssueBoardItemReadOnly = ({
-    data, 
+    data: {id, title, text, tags}, 
     handleClickDelete, 
     handleClickEdit
   }) => {
     return (
-      <li className="IssueBoardItem" key={data.id}>
-        <h1 className="IssueBoardItem-title">{data.title}</h1>
-        <p className="IssueBoardItem-text">{data.text}</p>
+      <li className="IssueBoardItem" key={id}>
+        <h1 className="IssueBoardItem-title">{title}</h1>
+        <p className="IssueBoardItem-text">{text}</p>
         <ul className="IssueBoardItem-tags">
           <li className="IssueBoardItem-tagsTitle">tags:</li>
-          {data.tags.map(tag => tag && <li><a className="Button Button-tagDisplay">{tag}</a></li>)}
+          {tags.map(tag => tag && <li><a className="Button Button-tagDisplay">{tag}</a></li>)}
         </ul>
         <div className="IssueBoardItem-buttonsWrapper">
-          <a onClick={() => handleClickEdit(data.id)} className="Button">edit</a>
-          <a onClick={() => handleClickDelete(data.id)} className="Button">delete</a>
+          <a onClick={() => handleClickEdit(id)} className="Button">edit</a>
+          <a onClick={() => handleClickDelete(id)} className="Button">delete</a>
         </div>
     </li>
     )
